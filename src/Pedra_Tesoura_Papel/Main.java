a0e2e9e17f206e5499f946a502fa0c2badc639ba
Leonardo <leonardo_rangel.santos@outlook.com>
2020-09-19 21:34:12 -0300

    Scanner!
    Scanner adicionado, para o user poder escolher sua jogada!

--- src/Pedra_Tesoura_Papel/Main.java
+++ src/Pedra_Tesoura_Papel/Main.java
@@ -1,14 +1,20 @@
 package Pedra_Tesoura_Papel;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main (String[] args){
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escolha 'pedra', 'papel', ou 'tesoura'");
+        String jogada = scanner.nextLine();
         Jogador joao = new Jogador("Joao","pedra");
         Jogo jogao = new Jogo(joao);
 
         jogao.Jogar();
         System.out.println(jogao.getResultado());
+
     }
 
 }
